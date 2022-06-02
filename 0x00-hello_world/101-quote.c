4c0e6546001c181f4a2f643b1ce78325f45a49bd
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-01 22:28:41 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str1[] = "and that piece is art is useful\" - Dora Korpar, 2015-10-19\n";
+        
+	write(2, str1, 50);
+	return (1);
+}

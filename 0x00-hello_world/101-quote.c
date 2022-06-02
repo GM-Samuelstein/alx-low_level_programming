24c20052b3c30aeb95daa204a64d6630ce16584e
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-01 22:33:13 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,6 +9,6 @@ int main(void)
 {
 	char str1[] = "and that piece is art is useful\" - Dora Korpar, 2015-10-19\n";
         
-	write(2, str1, 50);
+	write(2, str1, 59);
 	return (1);
 }

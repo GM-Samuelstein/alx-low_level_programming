69fe04644911315b4b25e073d5fa718a031b8fc6
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-03 17:42:47 -0700

    Numbers game

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This programme prints all single digit numbers of base 10,
+ * starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar ('\n');
+	return (0);
+}

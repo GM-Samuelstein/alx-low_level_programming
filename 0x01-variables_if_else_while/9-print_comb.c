fb6b06f4151cf86df1ebaac34b05a68bbaf74b37
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-03 19:30:33 -0700

    Combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - This programme prints all possible combinations
+ * of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

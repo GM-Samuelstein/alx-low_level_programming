567e2e242540c04e28ca0b7c28b5089825962506
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-07 14:42:08 -0700

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - Computes and prints the sum of all the multiples of '3' or '5'
+ * below '1024' (excluded), followed by a new line.
  *
- *
+ * Return: Always 0 (Success)
  */
 
 int main(void)

c6afbb5f99c8b71628c2cb60a5afec97e06ae79d
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-07 00:39:16 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	
 	int i, a;
 
 	a = sizeof(str) / sizeof(int);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,3 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
-

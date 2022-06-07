365897e00ee1a0bb75438c4b880aeec9a9b7514e
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-07 01:34:23 -0700

    alphabetic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,11 @@
-#include "school.h"
+#include "main.h"
 
 /**
+ * _isalpha - Checks for alphabetic character.
  *
+ * @c: ASCII character.
  *
+ * Return: 1 if c is a letter, lowercase or uppercase. 0 if otherwise.
  */
 
 int _isalpha(int c)

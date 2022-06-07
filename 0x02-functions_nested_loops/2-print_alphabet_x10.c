38b7d2cc117e15a095f7284d1a27a7f3cc6ad3f2
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-07 01:07:58 -0700

    ten folds

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
-#include "school.h"
+#include "main.h"
 
 /**
- * main -
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 
-void main(void)
+void print_alphabet_x10(void)
 {
-	int a, b;
+	int a, i;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 97; b <= 122; b++)
+		for (i = 97; i <= 122; i++)
 		{
-			_putchar(b);
+			_putchar(i);
 		}
 	_putchar('\n');
 	}

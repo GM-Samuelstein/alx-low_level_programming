c34693272555d9c729b5e6129acfd21401ae00a7
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-09 15:52:00 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle.
+ * @size: Size of the triangle.
+ * Return: Always 0 (success).
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar (' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}

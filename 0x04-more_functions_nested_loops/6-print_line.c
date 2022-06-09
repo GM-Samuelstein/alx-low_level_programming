f6e4574db7c2d01c4b6450e9bdecdf63b0eb1eb7
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-09 14:46:15 -0700

    long line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,7 @@
 /**
  * print_line - Draws a straight line on the terminal,
  * by printing the character '_' n times.
+ * @n: Number of times the character '_' should be printed.
  * Return: Always 0 (success).
  */
 

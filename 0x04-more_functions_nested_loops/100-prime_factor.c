05001a14e147f4120069275a9544a413f9d1edda
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-09 16:07:11 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the largest prime factor.
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	long int n, i;
+
+	n = 612852475143;
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			i--;
+		}
+	}
+	printf("%ld\n", i);
+	return (0);
+}

7d2b6964bd7ed97eb308a26127872f8b194d6c98
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-09 14:32:33 -0700

    long line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line on the terminal,
+ * by printing the character '_' n times.
+ * Return: Always 0 (success).
+ */
+
+void print_line(void)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+}

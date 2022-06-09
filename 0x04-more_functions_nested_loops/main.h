7fad37be4af476925c5509ac48f89e450068ed5a
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-09 10:40:52 -0700

    caps or not?

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int _isupper(int c);
 int _isdigit(int c);
-int mul((int a, int b);
+int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char);
 void print_most_numbers(void);
@@ -15,3 +15,4 @@ void print_triangle(int size);
 void print_number(int n);
 
 #endif
+

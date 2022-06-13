43f43c341b8532149c9068226bac5e382ae69c33
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-13 02:49:41 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ * @n: Input integer.
+ * Return: Always 0 (Success).
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}

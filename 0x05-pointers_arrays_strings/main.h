d003c008c52e3e69216e533bef9131487b425e3b
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-13 03:16:26 -0700

    3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);

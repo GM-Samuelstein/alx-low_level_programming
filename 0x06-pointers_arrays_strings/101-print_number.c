667873c629a372630e373ed610ef30068f19abf6
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-15 07:33:12 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: Inpuut integer.
+ * Return: No return.
+ */
+
+void print_number(int n)
+{
+	unsigned int a, b, x;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		a = n * -1;
+	}
+	else 
+	{
+		a = n;
+	}
+
+	b = a;
+	x = 1;
+
+	while (b > 9)
+	{
+		b /= 10;
+		x *= 10;
+	}
+	for (; x >= 1; x /= 10)
+	{
+		_putchar(((a / x) % 10) + 48);
+	}
+}

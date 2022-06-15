693cb58276a7e6322c53dcf2d205b00bb6d95bd3
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-15 07:28:56 -0700

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: Input string.
+ * Return: The pointer to dest.
+ */
+
+char *rot13(char *s)
+{
+	int x = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcefghilklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + x) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + x) == alphabet[i])
+			{
+				*(s + x) = rot13[i];
+				break;
+			}
+		}
+		x++;
+	}
+	return (s);
+}

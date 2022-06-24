d778a82f5ca6a42a6e00bb0b67a7ab3d78ff800d
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-06-23 22:45:40 -0700

    Argc Argv

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">0x0A: ARGC ARGV</h1>
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+
+#endif

3f26ad06e6c0bba7212f5a5d3777a612783731af
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-07-05 09:52:58 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}

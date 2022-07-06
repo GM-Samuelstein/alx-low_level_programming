d2032e849ec8d5eab7cc66b8bb3f433a5923d8f0
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-07-06 05:32:25 -0700

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, numberOfBytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	numberOfBytes = atoi(argv[1]);
+
+	if (numberOfBytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < numberOfBytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != numberOfBytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}

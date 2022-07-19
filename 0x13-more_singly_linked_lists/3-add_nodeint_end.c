604af3cb5d9b934692f820b91fe6c5d781c9ef7d
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-07-19 01:30:10 -0700

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
-		return(NULL);
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;

4e3b321c46a8914f413dea1acbd89803fc461f55
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-11-10 10:12:53 +0100

    Pycodestyle validation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,14 +14,14 @@ def island_perimeter(grid):
             0 represents a water zone.
             1 represents a land zone.
             One cell is a square with side length 1.
-            Grid cells are connected horizontally/vertically 
+            Grid cells are connected horizontally/vertically
             (not diagonally).
             Grid is rectangular, width and height don’t exceed 100.
 
     Returns:
         The perimeter of the island defined in grid.
     """
-    
+
     width = len(grid[0])
     height = len(grid)
     edges = 0
